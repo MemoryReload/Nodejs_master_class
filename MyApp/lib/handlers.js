67167cd71fff0a61a38cb1ece41d1a7eb39e7845
MemoryReload <heping_tsdwx@163.com>
2019-03-01 00:09:11 +0800

    bufix in handler.js

--- MyApp/lib/handlers.js
+++ MyApp/lib/handlers.js
@@ -68,82 +68,98 @@ handlers._users.POST = function ( data, callback ) {
 
 //TODO: authentication
 handlers._users.GET = function ( data, callback ) {
-  var phone = typeof(data.query.phone) == "string" && data.query.phone.length == 11 ? data.query.phone :false;
-  if (phone) {
-    _data.read("users",phone,(error, data) =>{
-      if (!error) {
-        //remove the password, do not show them to the user.
-        delete data.password;
-        callback(200, data);
-      } else {
-        callback(404, {"Error":"The specified user not found!"});
-      }
-    });
-  } else {
-    callback(200,{"Error":"Missing required fields!"});
-  }
+    var phone = typeof ( data.query.phone ) == "string" && data.query.phone.length == 11 ? data.query.phone : false;
+    if ( phone ) {
+        _data.read( "users", phone, ( error, data ) => {
+            if ( !error ) {
+                //remove the password, do not show them to the user.
+                delete data.password;
+                callback( 200, data );
+            } else {
+                callback( 404, {
+                    "Error": "The specified user not found!"
+                } );
+            }
+        } );
+    } else {
+        callback( 200, {
+            "Error": "Missing required fields!"
+        } );
+    }
 };
 
 //TODO: authentication
 handlers._users.PUT = function ( data, callback ) {
-  var payload = data.payload;
-  var phone = typeof ( data.payload.phone ) == "string" && data.payload.phone.trim()
-      .length == 11 ? data.payload.phone : false;
-  var password = typeof ( data.payload.password ) == "string" && data.payload.password.trim()
-      .length >= 8 ? data.payload.password : false;
-  var firstName = typeof ( data.payload.firstName ) == "string" && data.payload.firstName.trim()
-      .length > 0 ? data.payload.firstName : false;
-  var lastName = typeof ( data.payload.lastName ) == "string" && data.payload.lastName.trim()
-      .length > 0 ? data.payload.lastName : false;
-      if (phone) {
-        _data.read("users",phone,(error,data)=>{
-          if (!error) {
-            var newUser = data;
-            if (password) {
-              newUser.password = helpers.hashStr(password);
+    var payload = data.payload;
+    var phone = typeof ( data.payload.phone ) == "string" && data.payload.phone.trim()
+        .length == 11 ? data.payload.phone : false;
+    var password = typeof ( data.payload.password ) == "string" && data.payload.password.trim()
+        .length >= 8 ? data.payload.password : false;
+    var firstName = typeof ( data.payload.firstName ) == "string" && data.payload.firstName.trim()
+        .length > 0 ? data.payload.firstName : false;
+    var lastName = typeof ( data.payload.lastName ) == "string" && data.payload.lastName.trim()
+        .length > 0 ? data.payload.lastName : false;
+    if ( phone ) {
+        _data.read( "users", phone, ( error, data ) => {
+            if ( !error ) {
+                var newUser = data;
+                if ( password ) {
+                    newUser.password = helpers.hashStr( password );
+                }
+                if ( firstName ) {
+                    newUser.firstName = firstName;
+                }
+                if ( lastName ) {
+                    newUser.lastName = lastName;
+                }
+                _data.update( newUser, "users", phone, ( error ) => {
+                    if ( !error ) {
+                        callback( 200 );
+                    } else {
+                        callback( 500, {
+                            "Error": "Could not update the user!"
+                        } );
+                    }
+                } );
+            } else {
+                callback( 200, {
+                    "Error": "The specified user not found!"
+                } );
             }
-            if (firstName) {
-              newUser.firstName = firstName;
-            }
-            if (lastName) {
-              newUser.lastName = lastName;
-            }
-            _data.update(newUser,"users",phone,(error)=>{
-              if (!error) {
-                callback(200);
-              } else {
-                callback(500,{"Error":"Could not update the user!"});
-              }
-            });
-          } else {
-            callback(200,{"Error":"The specified user not found!"});
-          }
-        });
-      } else {
-        callback(200,{"Error":"Missing required filed!"});
-      }
+        } );
+    } else {
+        callback( 200, {
+            "Error": "Missing required filed!"
+        } );
+    }
 };
 
 //TODO: authentication
 handlers._users.DELETE = function ( data, callback ) {
-  var phone = typeof(data.query.phone) == "string" && data.query.phone.length == 11 ? data.query.phone :false;
-  if (phone) {
-    _data.read("users",data,(erro,data)=>{
-      if (!erro) {
-        _data.delete("users",phone,(erro)=>{
-          if (!erro) {
-            callback(200);
-          } else {
-            callback(500,{"Error":"Could not delete the user!"});
-          }
-        });
-      } else {
-        callback(200,{"Error":"The specified user not found!"});
-      }
-    });
-  } else {
-    callback(200,{"Error":"Missing required fields!"});
-  }
+    var phone = typeof ( data.query.phone ) == "string" && data.query.phone.length == 11 ? data.query.phone : false;
+    if ( phone ) {
+        _data.read( "users", phone, ( erro, data ) => {
+            if ( !erro ) {
+                _data.delete( "users", phone, ( erro ) => {
+                    if ( !erro ) {
+                        callback( 200 );
+                    } else {
+                        callback( 500, {
+                            "Error": "Could not delete the user!"
+                        } );
+                    }
+                } );
+            } else {
+                callback( 200, {
+                    "Error": "The specified user not found!"
+                } );
+            }
+        } );
+    } else {
+        callback( 200, {
+            "Error": "Missing required fields!"
+        } );
+    }
 };
 
 
